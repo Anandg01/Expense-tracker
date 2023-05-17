ea99a8d592588c287708107d1bbf1eb02696f731
Anandg01 <ptanandg@gmail.com>
2023-05-17 13:44:43 +0530

    Sending Reset Password Link

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require('express')
 const sequelize=require('./util/database')
 const userRout=require('./router/user')
+const resetpasword=require('./router/resetpassword')
 const bodyParser=require('body-parser')
 const cors=require('cors')
 const app=express();
@@ -9,13 +10,18 @@ const User=require('./models/user')
 const Expance=require('./models/expance')
 const order=require('./models/orders')
 const expanceRout=require('./router/expance')
-
+const Forgotpassword=require('./models/resetpassword')
 app.use(cors())
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(express.static('views'));
+
+
 app.use('/user',userRout)
+app.use('/password',resetpasword)
+
 app.use(expanceRout)
 app.use((req, res)=>{
     res.send(`<h1>Not a page</h1>`)
@@ -26,6 +32,9 @@ Expance.belongsTo(User)
 
 User.hasMany(order);
 order.belongsTo(User);
+
+User.hasMany(Forgotpassword)
+Forgotpassword.belongsTo(User)
 
 sequelize
 .sync()
--- router/user.js
+++ router/user.js
@@ -1,10 +1,7 @@
 const express=require('express')
 const userControler=require('../controler/user')
-const fargotconstrol=require('../controler/forgetpassword')
 const router=express.Router();
 router.post("/signUp",userControler.postData)
 router.get('/get',userControler.getData)
 router.post('/login',userControler.login)
-
-router.post('/forgetpassword',fargotconstrol.sendmail)
 module.exports=router;
